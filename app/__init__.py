bdba445ae93b98984c19915cc41e31d4859b2888
Charlotte Natasha <natashaonyango97@gmail.com>
2022-04-27 23:26:19 +0200

    added pictures

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,6 @@
 from flask import Flask
 from .config import DevConfig
 from flask_bootstrap import Bootstrap
-from app import views
-from app import error
 
 # Initializing application
 app = Flask(__name__,instance_relative_config = True)
@@ -15,3 +13,4 @@ app.config.from_pyfile('config.py')
 bootstrap = Bootstrap(app)
 
 from app import views
+from app import error
--- app/views.py
+++ app/views.py
@@ -3,20 +3,6 @@ from app import app
 from .request import get_movies, get_movie, search_movie
 from flask import render_template,request,redirect,url_for
 
-
-@app.route('/')
-def index():
-
-    '''
-    View root page function that returns the index page and its data
-    '''
-
-    # Getting popular movie
-    popular_movies = get_movies('popular')
-    print(popular_movies)
-    title = 'Home - Welcome to The best Movie Review Website Online'
-    return render_template('index.html', title = title,popular = popular_movies)
-
 @app.route('/movies')
 def movies():
 
